c3c16ce247a0f22ace30c9dba85fefe5791310e8
Mark Moran <mark.alxndr8@gmail.com>
2023-06-04 00:56:50 -0500

    conecting js with json, editing html

--- js/main.js
+++ js/main.js
@@ -7,46 +7,53 @@ const quantityCart = document.getElementById("quantityCart");
 
 let cart = JSON.parse(localStorage.getItem("dataCart")) || [];
 
-products.forEach((prod) => {
-    let content = document.createElement("div")
-    content.className = "gallery-products"
-    content.innerHTML = `
-    <img src="${prod.img}">
-    <h4>${prod.name}</h4>
-    <p class="price">S/${prod.price}</p>
-    `;
+const getProd = async () => {
+    const response = await fetch("../js/data.json");
+    const data = await response.json();
 
-    shopContent.append(content);
-
-    let shop = document.createElement("button");
-    shop.innerText = " Comprar ";
-    shop.className = "shop-btn bx-cart"
-
-    content.append(shop);
-
-    shop.addEventListener("click", () => {
-    const repeat = cart.some((repeatProduct) => repeatProduct.id === prod.id);
-
-    if (repeat){
-        cart.map ((product) => {
-            if (product.id === prod.id){
-                product.quantity++;
-            }
+    data.forEach((prod) => {
+        let content = document.createElement("div")
+        content.className = "gallery-products"
+        content.innerHTML = `
+        <img src="${prod.img}">
+        <h4>${prod.name}</h4>
+        <p class="price">S/${prod.price}</p>
+        `;
+    
+        shopContent.append(content);
+    
+        let shop = document.createElement("button");
+        shop.innerText = " Comprar ";
+        shop.className = "shop-btn bx-cart"
+    
+        content.append(shop);
+    
+        shop.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === prod.id);
+    
+        if (repeat){
+            cart.map ((product) => {
+                if (product.id === prod.id){
+                    product.quantity++;
+                }
+            });
+        } else{
+            cart.push({
+                id: prod.id,
+                img: prod.img,
+                name: prod.name,
+                price: prod.price,
+                quantity: prod.quantity,
+            });
+        }
+            console.log(cart);
+            cartCounter();
+            saveLocal();
         });
-    } else{
-        cart.push({
-            id: prod.id,
-            img: prod.img,
-            name: prod.name,
-            price: prod.price,
-            quantity: prod.quantity,
-        });
-    }
-        console.log(cart);
-        cartCounter();
-        saveLocal();
     });
-});
+};
+
+getProd();
 
 //! Storage: almacenar datos
 const saveLocal = () => {
